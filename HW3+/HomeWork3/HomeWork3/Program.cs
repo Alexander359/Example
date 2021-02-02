92610f251a6505e2653a22dd2067f8094339e359
Alexander359 <77802414+Alexander359@users.noreply.github.com>
2021-02-02 11:03:16 +0300

    Alexander
    Выполнение ДЗ 3

---
+++ HW3+/HomeWork3/HomeWork3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Поменяйте редактор по умолчанию в GitBash");
+            Console.WriteLine("git config --global core.editor \"editor--parameters\" ");
+            Console.WriteLine("создайте тренировочный репозиторий локально");
+            Console.WriteLine("git init");
+            Console.WriteLine("добавьте файл");
+            Console.WriteLine("git add . - добавить все измененные или новые файлы в репозитарий");
+            Console.WriteLine("сделайте коммит");
+            Console.WriteLine("git commit -m \"Текст комента\"");
+            Console.WriteLine("привяжите репозиторий к уддаленому репозиторию");
+            Console.WriteLine("git remote add origin https://github.com/Alexander359/Example.git");
+            Console.WriteLine("выкачайте в другую папку копию созданного уддаленного репозитория");
+            Console.WriteLine("git clone https://github.com/RusyaOne/CSharpAdvanced_19.01.2021.git");
+            Console.WriteLine("Создайте репозиторий с ДЗ, добавьте ДЗ в Google Class");
+            Console.WriteLine("ДЗ создан, а гуглу - вырвать бы глазз");
+        }
+    }
+}
