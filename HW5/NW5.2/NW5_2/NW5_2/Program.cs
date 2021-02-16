492dae07bd67e5fc006702d93099904b2103d695
Alexander359 <77802414+Alexander359@users.noreply.github.com>
2021-02-16 08:38:03 +0300

    Alexander
    Home Work 5.2
    16.02.21

--- HW5/NW5.2/NW5_2/NW5_2/Program.cs
+++ HW5/NW5.2/NW5_2/NW5_2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HW5_2;
 
 namespace NW5_2
 {
@@ -6,10 +7,34 @@ namespace NW5_2
     {
         static void Main(string[] args)
         {
+
+
+            Neighborhood N1 = new Neighborhood("Smirnoff", "WallStreet", 33, "New York", "New York", "USA");
+            Neighborhood N2 = new Neighborhood("Petroff", "East River", 41, "New York", "New York", "USA");
+            Neighborhood N3 = new Neighborhood("Sidoroff", "Broadway", 24, "New York", "New York", "USA");
+
+            N1.DisplayCountry();
+            N1.DisplayDistrict();
+            N1.DisplayCity();
+            N1.DisplayNeighborhood();
+
+            Console.WriteLine("====================================================");
+
+
+            N2.DisplayCountry();
+            N2.DisplayDistrict();
+            N2.DisplayCity();
+            N2.DisplayNeighborhood();
+
+            Console.WriteLine("====================================================");
+
+            N3.DisplayCountry();
+            N3.DisplayDistrict();
+            N3.DisplayCity();
+            N3.DisplayNeighborhood();
+
             
-            //Smirnoff
-            //Petroff
-            //Sidoroff
+            Console.ReadLine();
         }
     }
 }
